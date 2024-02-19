0217d192738b1b9d68aae78531cd2c6143805091
Pitra Pamungkas <pitra.adhi@gmail.com>
2024-02-19 13:48:03 +0700

    create generateRandomBetween function

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Title from '../components/Title';
 
+function generateRandomBetween(min, max, exclude) {
+  const rndNo = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNo === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+  return rndNo;
+}
+
 function GameScreen() {
   return (
     <View style={styles.container}>
